74aec7dd2cd7033524f4906e34ecc959952b4aa5
Krishn <chiranjiv.shah783@gmail.com>
2025-07-15 12:18:12 +0530

    Removed unused dependencies

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config(); // Load environment variables from .env
 
+
 const app = express();
 app.use(express.json());
 
