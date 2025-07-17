a58bb971ddce04e12d5131e71369680bf5297845
Krishn <chiranjiv.shah783@gmail.com>
2025-07-17 12:17:49 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
-require('dotenv').config(); // Load environment variables from .env
-
+require('dotenv').config(); 
 
 const app = express();
 app.use(express.json());
